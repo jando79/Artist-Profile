eb61fd044d41225d4e8701e87f9648380c67a989
David Jandron <jandron1211@gmail.com>
2023-04-28 16:46:16 -0700

    WIP navbar

--- src/components/App.js
+++ src/components/App.js
@@ -2,64 +2,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from "react";
 import Header from "./Header";
-import {
-  MDBContainer,
-  MDBNavbar,
-  MDBNavbarBrand,
-  MDBNavbarToggler,
-  MDBNavbarNav,
-  MDBNavbarItem,
-  MDBNavbarLink,
-  MDBCollapse
-} from 'mdb-react-ui-kit';
+import NavBar from "./NavBar";
 
-function App() {
-  const [showNav, setShowNav] = useState(false);
-
+function App(){
   return (
-    <Header />,
-    <MDBNavbar expand='lg' light bgColor='light'>
-      <MDBContainer fluid>
-        <MDBNavbarBrand href='#'>Navbar</MDBNavbarBrand>
-        <MDBNavbarToggler
-          type='button'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-          onClick={() => setShowNav(!showNav)}
-        >
-          {/* <MDBIcon icon='bars' fas /> */}
-        </MDBNavbarToggler>
-        <MDBCollapse navbar show={showNav}>
-          <MDBNavbarNav>
-            <MDBNavbarItem>
-              <MDBNavbarLink active aria-current='page' href='#'>
-                Home
-              </MDBNavbarLink>
-            </MDBNavbarItem>
-            <MDBNavbarItem>
-              <MDBNavbarLink href='#'>Features</MDBNavbarLink>
-            </MDBNavbarItem>
-            <MDBNavbarItem>
-              <MDBNavbarLink href='#'>Pricing</MDBNavbarLink>
-            </MDBNavbarItem>
-            <MDBNavbarItem>
-              <MDBNavbarLink disabled href='#' tabIndex={-1} aria-disabled='true'>
-                Disabled
-              </MDBNavbarLink>
-            </MDBNavbarItem>
-          </MDBNavbarNav>
-        </MDBCollapse>
-      </MDBContainer>
-    </MDBNavbar>
+  <React.Fragment>
+     <Header />
+     <NavBar />
+  </React.Fragment>
   );
 }
-// function App(){
-//   return (
-//   <React.Fragment>
-//      <Header />
-//      <Navigation />
-//   </React.Fragment>
-//   );
-// }
 
 export default App;
