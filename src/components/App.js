1a537a4ec684284434c8d9334a7a3894a8ab0bb1
David Jandron <jandron1211@gmail.com>
2023-04-28 13:48:51 -0700

    WIP reasearching graffiti fonts for header

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import Header from "./Header";
 
 function App(){
-  <>
+  return (
+  <React.Fragment>
      <Header />
-  </>
+  </React.Fragment>
+  );
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,9 @@ import React from "react";
 function Header () {
 
   return (
-    <>
-      <h1 style={{ textAlign: "center", fontSize: "50px", fontFamily: 'graffiti font' }}>DAVID JANDRON</h1>
-    </>
+    <React.Fragment>
+      <h1 style={{ textAlign: "center", fontSize: "50px"> }}>DAVID JANDRON</h1>
+    </React.Fragment>
   );
 }
 
