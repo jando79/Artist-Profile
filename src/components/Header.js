72fbddc6f76172efb2faf3d5170b5974799599be
David Jandron <jandron1211@gmail.com>
2023-04-28 12:09:55 -0700

    ADD components, header, and src edits

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header () {
+
+  return (
+    <>
+      <h1 style={{ textAlign: "center", fontSize: "50px", fontFamily: 'graffiti font' }}>DAVID JANDRON</h1>
+    </>
+  );
+}
+
+export default Header;
