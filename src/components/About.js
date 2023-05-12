aef4d2d8891e331f5bdd889926b814ff3ba2f123
David Jandron <jandron1211@gmail.com>
2023-05-12 10:54:07 -0700

    Change from complete site auth to allow read only without authentication

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,4 @@
 import React from "react";
-//import { Link } from "react-router-dom";
-//import styled from 'styled-components';
 import artistImage from "./../img/art-profile-pic.jpg";
 
 function About () {
