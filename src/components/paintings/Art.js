18f46368a3487ef3a9fb8c4a548e23b9c1b0cb6a
David Jandron <jandron1211@gmail.com>
2023-05-14 13:28:34 -0700

    WIP image bugfix

--- src/components/paintings/Art.js
+++ src/components/paintings/Art.js
@@ -3,11 +3,14 @@ import PropTypes from "prop-types";
 
 function Art(props){
 
+  const imgUrl = <img src={props.imgUrl} style={{ width: 100, height: 100 }} alt="painting" />;
+
   return (
     <React.Fragment>
       <div onClick = {() => props.whenArtClicked(props.id)}>
+        {imgUrl}
         {/* <img src={props.image} alt="painting" /> */}
-        <img src={props.imgUrl} style={{ width: 100, height: 100 }} alt="painting" />
+        {/* <img src={props.imgUrl} style={{ width: 100, height: 100 }} alt="painting" /> */}
         <h3>{props.title}</h3>
         <hr/>
       </div>
