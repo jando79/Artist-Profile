97738bd9acaa7c6160c4e716716ffdda64e24cc3
David Jandron <jandron1211@gmail.com>
2023-05-15 14:42:26 -0700

    WIP adding style and images to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,8 @@ function Home() {
     <React.Fragment>
       {/* <h3>filler</h3>
       <p>Some stuff is going to go here</p> */}
-      <img src={homeImage} className= "center" style={{ height: 350, width: 500, }} alt="Black And White" />
+      <img src={homeImage} className= "center" style={{ height: 350, width: 500, float: "left" }} alt="Black And White" />
+      <h3>Outsider Artist</h3>
     </React.Fragment>
   );
 }
