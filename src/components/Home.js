42109e98b419b25d2c87452441f8eb47902dc3cc
David Jandron <jandron1211@gmail.com>
2023-05-15 15:49:56 -0700

    ADD images to home and style

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,21 @@
 import React from "react";
-import homeImage from "./../img/BlackAndWhite.JPG";
+import homeImage1 from "./../img/BlackAndWhite.JPG";
+// import homeImage2 from "./../img/paintingHome.jpg";
+import homeImage3 from "./../img/collageHome.jpg";
+import homeImage2 from "./../img/paintingLoud.jpg";
 
 function Home() {
   return (
     <React.Fragment>
       {/* <h3>filler</h3>
       <p>Some stuff is going to go here</p> */}
-      <img src={homeImage} className= "center" style={{ height: 350, width: 500, float: "left" }} alt="Black And White" />
-      <h3>Outsider Artist</h3>
+      <img src={homeImage3} style={{ height: 350, width: 325, float: "left", paddingRight: 22 }} alt="Collage" />
+      <img src={homeImage2} style={{ height: 350, width: 500, float: "right"}} alt="Bright Painting" />
+      {/* <img src={homeImage2} style={{ height: 350, width: 275, float: "right", paddingLeft: 2 }} alt="Painting" /> */}
+      <img src={homeImage1} style={{ height: 350, width: 500 }} alt="Black And White" />
+      <br></br>
+      <center><h2>An Outsider Artist, specializing in Mixed-Media Paintings and Collage</h2></center>
+      
     </React.Fragment>
   );
 }
